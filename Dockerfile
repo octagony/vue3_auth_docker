976dc298f73e5def557c1abcd3643970314a5c6a
octagony <moctagony@gmail.com>
2023-11-11 12:40:35 +0300

    [REFACTOR] Refactor docker files

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,11 @@
 FROM node:18.18.2-alpine3.17 
 
-RUN npm install -g http-server
-
 WORKDIR /app
 
-COPY package*.json ./
+COPY ./package.json .
 
 RUN npm install
 
 COPY . .
 
-RUN npm run build
-
-EXPOSE 5173
-
-CMD ["http-server", "dist", "-a", "172.18.0.2", "-p", "3001"]
+CMD [ "npm" , "run", "dev" ] 
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import { fileURLToPath, URL } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    watch: {
+      usePolling: true
+    },
+    host: true,
+    port: 5173,
+    strictPort: true
+  },
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("./src", import.meta.url)),
